889755b7a12ac899d88014c0e1a5989247135676
Rahmaakram <akram.rahma0774@gmail.com>
2025-04-09 01:40:23 +0200

    index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/user');
+const authRoute = require("./auth");
+
+const userRoute = require("./user");
 
 dotenv.config();
 const app = express();
